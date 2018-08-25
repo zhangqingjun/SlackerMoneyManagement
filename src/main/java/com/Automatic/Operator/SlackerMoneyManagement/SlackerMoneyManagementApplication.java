e6a141441d04318290d421eb8dabe64be5c29849
zhangfei <1490427952@qq.com>
2018-08-25 16:21:18 +0800

    download project

---
+++ src/main/java/com/Automatic/Operator/SlackerMoneyManagement/SlackerMoneyManagementApplication.java
@@ -0,0 +1,12 @@
+package com.Automatic.Operator.SlackerMoneyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackerMoneyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackerMoneyManagementApplication.class, args);
+	}
+}
