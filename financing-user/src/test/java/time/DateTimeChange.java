6e8764583503a3f807be1f48d67d6c28ca02ab0e
zhangfei <1490427952@qq.com>
2019-04-21 22:17:14 +0800

    LocaDateTime

---
+++ financing-user/src/test/java/time/DateTimeChange.java
@@ -0,0 +1,13 @@
+package time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeChange {
+    public static void main(String[] args){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String nowTime = localDateTime.format(dateTimeFormatter);
+        System.out.println(nowTime);
+    }
+}
