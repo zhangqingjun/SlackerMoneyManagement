6e47f6940252f2c92d62221e54dc9e8085a871c9
zhangfei <1490427952@qq.com>
2019-05-23 10:34:32 +0800

    run config

--- financing-start/src/main/java/com/automatic/financing/FinancingApplication.java
+++ financing-start/src/main/java/com/automatic/financing/FinancingApplication.java
@@ -1,10 +1,10 @@
 package com.automatic.financing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FinancingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FinancingApplication.class, args);
     }
